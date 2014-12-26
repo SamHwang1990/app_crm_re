afd73135bfb167dc0056c544358889e3b177f178
SamHwang1990 <samhwang1990@gmail.com>
2014-12-26 15:01:28 +0800

    complete gulp to watch stylus files, and auto transform to css file

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,25 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var nib = require('nib');
 var sourcemaps = require('gulp-sourcemaps');
+var path = require('path');
+
+var config = require('./config');
+
+gulp.task('stylus',function(){
+	return gulp.src('./stylus/appcrm.styl')
+		.pipe(stylus({
+			use:nib(),
+			compress:true,
+			sourcemap: {
+				inline: true,
+				sourceRoot: '..',
+				basePath: 'css'
+			}
+		}))
+		.pipe(gulp.dest('./public/src/assets/css'));
+});
+
+gulp.task('default',['stylus'],function(){
+	return gulp.watch(['./stylus/*',config.server.distFolder + 'assets/imgs/*'],['stylus']);
+});
+
