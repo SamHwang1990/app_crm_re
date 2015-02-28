184f9c4c02e0c3577760d6a7d57f8ac5025ce3f7
SamHwang1990 <samhwang1990@gmail.com>
2015-03-01 00:02:49 +0800

    fuck config phase

---
+++ bll/site.js
@@ -0,0 +1,5 @@
+/**
+ * Created by sam on 15-2-28.
+ */
+
+var getLocateModule = require('../utils/getLocate')();
--- public/src/common/security/login/login.js
+++ public/src/common/security/login/login.js
@@ -2,15 +2,16 @@
  * Created by sam on 15-2-6.
  */
 
-angular.module('security.login', [], ['$routeProvider', '$rootScope', function($routeProvider, $rootScope){
+angular.module('security.login', [])
 
-    // TODO: judge whether the user is login or logout
-  var templateUrl = 'security/login/login.' + $rootScope.local + '.tpl.html';
-  $routeProvider.when('/login', {
-    templateUrl: templateUrl,
-    controller: 'LoginPageController'
-  })
-}])
+.config(['$routeProvider', function($routeProvider){
+    var templateUrl = 'security/login/login.tpl.html';
+    $routeProvider.when('/login', {
+      templateUrl: templateUrl,
+      controller: 'LoginPageController'
+    });
+  }]
+)
 
 .controller('LoginPageController', ["$scope", function($scope){
     $scope.bodyClass = 'body-login';
